e95b584008674e7a254f6ee11e6f61c7ed247de5
Frahyang <142311241+Frahyang@users.noreply.github.com>
2024-01-12 23:04:39 +0700

    Add files via upload

---
+++ Others/Map Files/Space Tiles.tsx
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Space Tiles" tilewidth="64" tileheight="64" tilecount="37" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="Map Tile Images/pathTurn3To6Pair.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="Map Tile Images/pathTurn12To3Pair.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="Map Tile Images/pathTurn9To12.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="Map Tile Images/pathTurn6to9.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="Map Tile Images/pathTurn3to6.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="Map Tile Images/pathTurn12To3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="Map Tile Images/path5NorthSpace.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="Map Tile Images/path4NorthSpace.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="Map Tile Images/path3NorthSpace.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="Map Tile Images/path1NorthSpace.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="Map Tile Images/space.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="Map Tile Images/path5WestSpace.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="Map Tile Images/path4WestSpace.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="Map Tile Images/path3WestSpace.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="Map Tile Images/path2WestSpace.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="Map Tile Images/path1WestSpace.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="Map Tile Images/path5SouthSpace.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="Map Tile Images/path4SouthSpace.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="Map Tile Images/path3SouthSpace.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="Map Tile Images/path2SouthSpace.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="Map Tile Images/path1SouthSpace.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="Map Tile Images/path5EastSpace.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="Map Tile Images/path4EastSpace.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="Map Tile Images/path3EastSpace.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="Map Tile Images/path2EastSpace.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="Map Tile Images/path2NorthSpace.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="Map Tile Images/path1EastSpace.png"/>
+ </tile>
+ <tile id="27">
+  <image width="64" height="64" source="Map Tile Images/turretPlatform.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="64" source="Map Tile Images/pathTurn6To9Pair.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="Map Tile Images/pathTurn9To12Pair.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="Map Tile Images/portalFrame2.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="Map Tile Images/portalFrame4.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="64" source="Map Tile Images/portalFrame3.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="Map Tile Images/portalFrame1.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="Map Tile Images/spaceStar3.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="Map Tile Images/spaceStar2.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="Map Tile Images/spaceStar1.png"/>
+ </tile>
+</tileset>
